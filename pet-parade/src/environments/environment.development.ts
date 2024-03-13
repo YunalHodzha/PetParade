92aabce655fbef55cb5b6645ffbfaae1dcd6cd22
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2024-03-13 11:32:20 +0200

    add environments and add firebase config

---
+++ pet-parade/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyAt_pwF0jg6Piuk8-Gn7MoiW2bH_rXTQjQ",
+        authDomain: "pet-parade-90329.firebaseapp.com",
+        projectId: "pet-parade-90329",
+        storageBucket: "pet-parade-90329.appspot.com",
+        messagingSenderId: "1086225892745",
+        appId: "1:1086225892745:web:bec2b7c8ee365663a98d58"
+      }
+};
