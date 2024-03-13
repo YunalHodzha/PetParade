c67bdac8c9f3171ad8d96c0e428095dd76e67304
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2024-03-13 14:22:42 +0200

    add nav-bar and styles to home page, nav-bar

--- pet-parade/src/app/app.module.ts
+++ pet-parade/src/app/app.module.ts
@@ -9,14 +9,21 @@ import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
 import { environment } from 'src/environments/environment';
 import { HomeComponent } from './pages/home/home.component';
 
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {
   constructor() {
-    FirebaseTSApp.init(environment.firebaseConfig)
+    FirebaseTSApp.init(environment.firebaseConfig);
   }
 }
