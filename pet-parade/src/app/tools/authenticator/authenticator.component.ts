6346e5915235c711a6dd7d64facf95bb45358f88
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2024-03-15 15:55:21 +0200

    Now can toggle changing the state between login and register

--- pet-parade/src/app/tools/authenticator/authenticator.component.ts
+++ pet-parade/src/app/tools/authenticator/authenticator.component.ts
@@ -3,15 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-authenticator',
   templateUrl: './authenticator.component.html',
-  styleUrls: ['./authenticator.component.css']
+  styleUrls: ['./authenticator.component.css'],
 })
 export class AuthenticatorComponent implements OnInit {
   state = AuthenticatorCompState.LOGIN;
   constructor() {}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   onCreateAccountClick() {
     this.state = AuthenticatorCompState.REGISTER;
@@ -21,8 +19,13 @@ export class AuthenticatorComponent implements OnInit {
     this.state = AuthenticatorCompState.LOGIN;
   }
 
+  isLoginState() {
+    return this.state == AuthenticatorCompState.LOGIN;
+  }
 
-
+  isRegisterState() {
+    return this.state == AuthenticatorCompState.REGISTER;
+  }
 }
 
 export enum AuthenticatorCompState {
