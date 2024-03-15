697b851c6ffed15bd665ee80c7c08997efe0fc46
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2024-03-15 15:41:59 +0200

    Add AuthenticatorCompState
    For toggling between login and register functionality

--- pet-parade/src/app/tools/authenticator/authenticator.component.ts
+++ pet-parade/src/app/tools/authenticator/authenticator.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-authenticator',
   templateUrl: './authenticator.component.html',
   styleUrls: ['./authenticator.component.css']
 })
-export class AuthenticatorComponent {
+export class AuthenticatorComponent implements OnInit {
+  state = AuthenticatorCompState.LOGIN;
+  constructor() {}
 
+  ngOnInit(): void {
+    
+  }
+
+  onCreateAccountClick() {
+    this.state = AuthenticatorCompState.REGISTER;
+  }
+
+  onLoginClick() {
+    this.state = AuthenticatorCompState.LOGIN;
+  }
+
+
+
+}
+
+export enum AuthenticatorCompState {
+  LOGIN,
+  REGISTER,
 }
