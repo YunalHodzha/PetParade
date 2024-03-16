5ee4f19ab8bef04378deed223a4d6ad98ce05a79
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2024-03-16 10:14:01 +0200

    add user registration functionality with input validation
    onRegisterClick() function integrates with Firebase for account creation

--- pet-parade/src/app/tools/authenticator/authenticator.component.ts
+++ pet-parade/src/app/tools/authenticator/authenticator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
 
 @Component({
   selector: 'app-authenticator',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticatorComponent implements OnInit {
   state = AuthenticatorCompState.LOGIN;
-  constructor() {}
+
+  firebasetsAuth: FirebaseTSAuth; //To manage auth part of firebase
+
+  constructor() {
+    this.firebasetsAuth = new FirebaseTSAuth();
+  }
 
   ngOnInit(): void {}
+
+  onRegisterClick(
+    registerEmail: HTMLInputElement,
+    registerPassword: HTMLInputElement,
+    registerConfirmPassword: HTMLInputElement
+  ) {
+    let email = registerEmail.value;
+    let password = registerPassword.value;
+    let confirmPassword = registerConfirmPassword.value;
+
+    if (
+      this.isNotEmpty(email) &&
+      this.isNotEmpty(password) &&
+      this.isNotEmpty(confirmPassword) &&
+      this.isAmatch(password, confirmPassword)
+    ) {
+      this.firebasetsAuth.createAccountWith({
+        email: email,
+        password: password,
+        onComplete: (uc) => {
+          alert('Account Created!');
+          registerEmail.value = '';
+          registerPassword.value = '';
+          registerConfirmPassword.value = '';
+        },
+        onFail: (err) => {
+          alert('Failed to create the account.');
+        },
+      });
+    }
+  }
+
+  isNotEmpty(text: string) {
+    return text != null && text.length > 0;
+  }
+
+  isAmatch(text: string, compareWith: string) {
+    return text == compareWith;
+  }
 
   onCreateAccountClick() {
     this.state = AuthenticatorCompState.REGISTER;
