5fdff0e192dd2e0d8d4e654df99b49cee1d39f42
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2024-03-16 11:12:54 +0200

    Add real-time auth with FirebaseTSAuth, dynamic nav-bar updates
    Implemented sign-in/sign-out methods and a status checker to dynamically toggle "Login"/"Logout" in the nav-bar

--- pet-parade/src/app/app.component.ts
+++ pet-parade/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { AuthenticatorComponent } from './tools/authenticator/authenticator.component';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,30 @@ import { AuthenticatorComponent } from './tools/authenticator/authenticator.comp
 })
 export class AppComponent {
   title = 'pet-parade';
+  auth = new FirebaseTSAuth();
 
-  constructor(private loginSheet: MatBottomSheet) {}
-  
+  constructor(private loginSheet: MatBottomSheet) {
+    this.auth.listenToSignInStateChanges((user) => {
+      this.auth.checkSignInState({
+        whenSignedIn: (user) => {
+          alert('Logged In');
+        },
+        whenSignedOut: (user) => {
+          alert('Logged Out');
+        },
+        whenChanged: (user) => {},
+      });
+    });
+  }
+
+  onLogoutClick() {
+    this.auth.signOut();
+  }
+
+  loggedIn() {
+    return this.auth.isSignedIn();
+  }
+
   onLoginClick() {
     this.loginSheet.open(AuthenticatorComponent);
   }
